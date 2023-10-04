776e80e433c35a0ebc7a1d54240224d876127e57
dinhdanhnguyen <datvsdanh@gmail.com>
2023-10-04 11:26:09 -0700

    add

---
+++ Lab/Lab10/student.js
@@ -0,0 +1,41 @@
+function Student(firstName, lastName, studentID) {
+
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.studentID = studentID;
+
+    this.display = function () {
+
+        console.log(`${this.firstName}, ${this.lastName}, ${this.studentID}`);
+    }
+
+}
+
+ 
+
+var studentArray = [];
+
+studentArray.push(new Student("Dinh", "Nguyen", 0));
+studentArray.push(new Student("Rachel", "Green", 1));
+
+studentArray[1].display()
+
+
+let tan = new Student("Tan", "Nguyen", 4)
+tan.graduated = true
+console.log(tan)
+
+ 
+
+var Mark = {
+
+    firstName: "Mark",
+    lastName: "Tribianni",
+    studentID: 3,
+
+    __proto__: new Student
+
+};
+
+console.log(Mark)
+Mark.display()
